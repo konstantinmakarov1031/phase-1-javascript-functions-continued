fd51e58f8209d753d346169083f90b287a6282f9
konstantin <makarovestates@gmail.com>
2022-07-28 05:31:47 -0500

    commit tree 00295dea348bcd50f58c6dc4d24ec17a2431a142

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-function saturdayFun(activity = "roller-skate"){
+function saturdayFun(activity="roller-skate") {
     return `This Saturday, I want to ${activity}!`
-
-}
-
-function mondayWork(activity = "go to the office"){
+  }
+ 
+  let mondayWork = function(activity="go to the office") {
     return `This Monday, I will ${activity}.`
-}
-
-function wrapAdjective(highlight){
-    const innerFunction = function (declaration = "special"){
-        return `You are ${highlight}${declaration}${highlight}!`
-    };
-    return innerFunction;
-}
+  }
+ 
+  let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+ 
+  const Calculator = {
+    add: function(a,b) {
+      return a + b;
+    },
+    subtract: function(a,b) {
+      return a - b
+    },
+    multiply: function(a,b) {
+      return a * b
+    },
+    divide: function(a,b) {
+      return a / b;
+    }
+  }
+ 
+  let actionApplyer = function(start, ray) {
+    let a = start
+ 
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+ 
+    return a
+  }
